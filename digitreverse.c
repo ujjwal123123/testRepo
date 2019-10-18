5bf53c36cbe1f61a00509ac0ae2e53e0a4a5cbf0
ME-Devansh <devansh.pant02@gmail.com>
2019-10-19 00:07:06 +0530

    Corrected indentation

--- digitreverse.c
+++ digitreverse.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int n,no,s=0;
-printf("Enter the no you want to reverse");
-scanf("%d",&n);
-for(;n>0;)
-{
-	no=n%10;
-	n=n/10;
-	s=s*10+no;
+	int n, no, s=0;
+	printf("Enter the no you want to reverse: ");
+	scanf("%d",&n);
+
+	for(;n>0;) {
+		no=n%10;
+		n=n/10;
+		s=s*10+no;
+	}
+
+	printf("%d\n", s);
+	
+	return 0;
 }
-return 0;
